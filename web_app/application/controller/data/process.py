00780d95a60fa744c692a8c9aeca93a28be98fee
theshaftman <mariyan.apostolov89@gmail.com>
2024-05-29 20:36:03 +0300

    Prepare data for anonymization

--- web_app/application/controller/data/process.py
+++ web_app/application/controller/data/process.py
@@ -17,7 +17,8 @@ def data_upload_excel():
     # Parse the data as a Pandas DataFrame type
     data = pandas.read_excel(file) if (file.content_type in xlsx_content_types) else \
         pandas.read_csv(file, sep=',', encoding='unicode_escape', index_col=0, on_bad_lines="warn")
-    session['uploaded_file_data'] = data.to_html()
+    session['uploaded_file_data_html'] = data.to_html()
+    session['uploaded_file_data_json'] = data.to_json(force_ascii=False)
 
     # Return HTML snippet that will render the table
     return redirect('/')
